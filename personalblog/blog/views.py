79b5bdb8eb77a5158c3c329396db17a8e343458f
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-06 19:40:33 +0545

    added page for admin

--- personalblog/blog/views.py
+++ personalblog/blog/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Article
 
 def guest_home(request):
     return render(request, "guest/home.html")
@@ -8,8 +9,13 @@ def guest_home(request):
 
 @login_required
 def admin_dashboard(request):
-    ctx = {
-        "total_users": User.objects.count(),
-        "recent_signups": User.objects.order_by("-date_joined")[:5],
+    list_articles = Article.objects.all().order_by("created_at")
+    # ctx = {
+    #     "total_users": User.objects.count(),
+    #     "recent_signups": User.objects.order_by("-date_joined")[:5],
+    # }
+    display_article = {
+        "total_articles":Article.objects.count(),
+        "list_articles": list_articles,
     }
-    return render(request, "admin/dashboard.html", ctx)
+    return render(request, "admin/dashboard.html",display_article)
